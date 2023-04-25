79b03e7f6a8fb0a7996d40548a32b385fd5daf70
Karol Zięba <3zu_einfach@protonmail.com>
2023-04-25 22:10:25 +0200

    Experimenting

--- passproject.py
+++ passproject.py
@@ -1,29 +1,39 @@
 from string import printable, whitespace
 from random import sample, randint
+import csv
+import os.path
 
-class Password():
 
-    def __init__(self, length, website):
-        self.length = length
-        self.website = website
 
-    def generate_password(self):
-        printable_chars = printable.strip()
-        whitespace_chars = whitespace
-        non_whitespace_printable_chars = [c for c in printable_chars if c not in whitespace_chars]
-        chosen_chars_list = sample(non_whitespace_printable_chars, self.length)
-        generated_password = ''.join(chosen_chars_list)
-        return generated_password
+def generate_password(length):
+    printable_chars = printable.strip()
+    whitespace_chars = whitespace
+    non_whitespace_printable_chars = [c for c in printable_chars if c not in whitespace_chars]
+    chosen_chars_list = sample(non_whitespace_printable_chars, length)
+    generated_password = ''.join(chosen_chars_list)
+    return generated_password
         
 
-what_length = int(input("How long your password has to be?  "))
-what_website = input('What website do you need password for?  ')   
+choose_length = int(input("How long your password has to be?  "))
+website = input('What website do you need password for?  ')   
         
-password = Password(what_length, what_website)
-print(f'My password for {password.website} is {password.generate_password()} and its length is: {password.length}')
-f = open("passlist.text", "a")
-f.write(f'My password for {password.website} is {password.generate_password()} and its length is: {password.length}'+'\n')
-f.close()
+password = generate_password(choose_length)
+
+print(f'My password for {website} is {password} and its length is: {choose_length}')
+if os.path.isfile('passwords.csv'):
+    print('File exists')
+    with open('passwords.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([website, password, choose_length])
+    file.close()
+    
+else:
+    print('File does not exist')
+    with open('passwords.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([website, password, choose_length])
+    file.close()
+
 
 
 
